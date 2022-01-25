f1596df8047173ce6def11cfa7c2e04ea3f3a8c8
Kingslin0810 <88696779+Kingslin0810@users.noreply.github.com>
2022-01-24 18:56:25 -0800

    minor fix to the moles_grams_converter test

--- tests/testthat/chemcalculator.R
+++ tests/testthat/chemcalculator.R
@@ -1,3 +1,4 @@
+# Test the moles_grams_converter function for correct output
 test_that("moles_grams_converter() converts grams to moles", {
     expect_equal(moles_grams_converter("H2O", 18.01528, "grams"), 1, tolerance = 1)
 })
@@ -10,6 +11,7 @@ test_that("moles_grams_converter() errors if input is 'tons'", {
     expect_error(moles_grams_converter("H2O", 1, "tons"))
 })
 
+# Test the moles_grams_converter function for correct input
 test_that("moles_grams_converter() errors if input is not a chemical formula", {
     expect_error(moles_grams_converter("[H2O]", 1, "moles"))
     expect_error(moles_grams_converter("CuSO4-5H2O", 1, "moles"))
