910da25f7edc85568c19f3759a3e88852e7c569f
Kingslin0810 <qinglin0810@gmail.com>
2022-01-31 18:41:35 -0800

    add global.R to remove warnings

---
+++ R/globals.R
@@ -0,0 +1,3 @@
+utils::globalVariables(c("Freq", "AtomicMass", "Freq.x",
+                         "VALUE", "UOM", "median", "sd", "q75",
+                         "Freq.y", "elem_freq", "comp_freq"))
