49e25d41bd4be6fefc4b6b07f5634e3ae5dd8a11
Kingslin0810 <88696779+Kingslin0810@users.noreply.github.com>
2022-01-31 20:14:47 -0800

    fix typo

--- R/globals.R
+++ R/globals.R
@@ -1,3 +1,2 @@
 utils::globalVariables(c("Freq", "AtomicMass", "Freq.x",
-                         "VALUE", "UOM", "median", "sd", "q75",
                          "Freq.y", "elem_freq", "comp_freq"))
